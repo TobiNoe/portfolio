55d1f63fdd4de3aaca3523e1bb5936ed0fcf75f5
Tobias Nölle <tobias.noelle@icloud.com>
2024-08-10 18:38:45 +0200

    footer component html scss ts: edit form - input field with valid icon and invalid icon and info-text for first input field

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,23 +1,25 @@
+import { CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  btnDisabled:boolean = true;
-  policyChecked:boolean = false;
+  btnDisabled: boolean = true;
+  policyChecked: boolean = false;
+  isEmptyName: boolean = true;
+  isEmptyMail: boolean = true;
 
   contactData = {
     name: "",
     email: "",
     message: ""
   }
-
 
   checkPolicy() {
     this.policyChecked = !this.policyChecked;
