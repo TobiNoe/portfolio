e845216ed93fa1022ff66dec4b054e63c3edf1e5
Tobias Nölle <tobias.noelle@icloud.com>
2024-08-13 19:21:55 +0200

    generate and integrate AboutMeComponent and import this in landing-page

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    HeroComponent
+    HeroComponent,
+    AboutMeComponent
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
