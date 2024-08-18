f21189177034e0dbd5935a92944086c5ac54122a
Tobias Nölle <tobias.noelle@icloud.com>
2024-08-18 15:57:07 +0200

    generate and integrated animateArrowComponent with basic structure

---
+++ src/app/landing-page/animate-arrow/animate-arrow.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animate-arrow',
+  standalone: true,
+  imports: [],
+  templateUrl: './animate-arrow.component.html',
+  styleUrl: './animate-arrow.component.scss'
+})
+export class AnimateArrowComponent {
+  position:number = 3;
+}
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { AnimateArrowComponent } from "./animate-arrow/animate-arrow.component";
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     HeroComponent,
-    AboutMeComponent
-  ],
+    AboutMeComponent,
+    AnimateArrowComponent
+],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
