5aa866f4c7fb47c7afe2fbbcd5c87a7a6fc820f2
Tobias Nölle <tobias.noelle@icloud.com>
2024-08-20 18:27:23 +0200

    added img for skills to assets

--- src/app/shared/services/data-service.service.ts
+++ src/app/shared/services/data-service.service.ts
@@ -8,11 +8,43 @@ export class DataServiceService {
   mySkills = [
     {
       name: "Angular",
-      img: "./../../../../assets/img/mySkills/angular.png"
+      img: "./../../../../assets/img/mySkills/angular.png",
     },
     {
       name: "TypeScript",
       img: "./../../../../assets/img/mySkills/typescript.png"
+    },
+    {
+      name: "JavaScript",
+      img: "./../../../../assets/img/mySkills/javascript.png"
+    },
+    {
+      name: "HTML",
+      img: "./../../../../assets/img/mySkills/html.png"
+    },
+    {
+      name: "CSS",
+      img: "./../../../../assets/img/mySkills/css.png"
+    },
+    {
+      name: "Firebase",
+      img: "./../../../../assets/img/mySkills/firebase.png"
+    },
+    {
+      name: "Git",
+      img: "./../../../../assets/img/mySkills/git.png"
+    },
+    {
+      name: "Scrum",
+      img: "./../../../../assets/img/mySkills/scrum.png"
+    },
+    {
+      name: "Rest-Api",
+      img: "./../../../../assets/img/mySkills/rest_api.png"
+    },
+    {
+      name: "Material Design",
+      img: "./../../../../assets/img/mySkills/material_design.png"
     }
   ]
 
