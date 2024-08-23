9096e3971b6a2703ece786496b6880abe1c21381
Tobias Nölle <tobias.noelle@icloud.com>
2024-08-23 14:34:38 +0200

    implement function ts: onLoadImage-function implement in mySkillsComponent to get image width of loaded image and set width of relative positioned parent div to center image

--- src/app/landing-page/my-skills/my-skills.component.ts
+++ src/app/landing-page/my-skills/my-skills.component.ts
@@ -1,16 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject} from '@angular/core';
 import { DataServiceService } from '../../shared/services/data-service.service';
-import { style } from '@angular/animations';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
 export class MySkillsComponent {
 
   data = inject(DataServiceService);
+  imageWidths: number[] = [];
 
+  onImageLoad(index: number, event: Event) {
+    const img = event.target as HTMLImageElement;
+    this.imageWidths[index] = img.width;
+  }
 }
