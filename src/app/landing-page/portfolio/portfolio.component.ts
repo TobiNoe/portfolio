5cb715995137a9bad29388b5e88e06f2a3f8469f
Tobias Nölle <tobias.noelle@icloud.com>
2024-08-26 14:32:04 +0200

    implemented html scss ts: portfolioProjectComponent

--- src/app/landing-page/portfolio/portfolio.component.ts
+++ src/app/landing-page/portfolio/portfolio.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PortfolioProjectComponent } from './portfolio-project/portfolio-project.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [PortfolioProjectComponent],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
