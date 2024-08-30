62decd69156868c2324a1934975a1f81717121bb
Tobias Nölle <tobias.noelle@icloud.com>
2024-08-30 19:20:59 +0200

    change structure and move navbar in shared/components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    NavbarComponent,
     RouterOutlet,
     FooterComponent
   ],
