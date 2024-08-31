d64ed41e46203a13dd40c672bf444d0b45e129ee
Tobias Nölle <tobias.noelle@icloud.com>
2024-08-31 11:24:54 +0200

    navbarComponent: html scss ts added id for scrolling event and changed toggleMenu() to set a boolien varaible to show or hide Menu

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,17 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   hmenuImg:string = "./../../../../../assets/img/navbar/burger_1.png"
-  displayToggle:string = "display: none"
+  menuIsToggled:boolean = false;
+  
 
   toggleMenu(){
-    this.displayToggle = "display: flex";
+    this.menuIsToggled = !this.menuIsToggled;
   }
 }
