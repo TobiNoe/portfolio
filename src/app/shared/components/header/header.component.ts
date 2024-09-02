27cbd8176551cacbdb24a5bb6cff4f5172bcfb3c
Tobias Nölle <tobias.noelle@icloud.com>
2024-09-02 12:13:58 +0200

    generate header component

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  hmenuImg:string = "./../../../../../assets/img/navbar/burger_1.png"
+}
