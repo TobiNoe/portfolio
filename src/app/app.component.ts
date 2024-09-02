1e1c72023b1f8ddafbc566b6817594ec2faa9a33
Tobias Nölle <tobias.noelle@icloud.com>
2024-09-02 14:45:44 +0200

    headerComponent: html scss ts styled component ans integrated component in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    HeaderComponent,
     RouterOutlet,
     FooterComponent
   ],
