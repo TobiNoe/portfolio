30021052d39b05f4702874e0d840bf604f77a5f1
Tobias Nölle <tobias.noelle@icloud.com>
2024-09-03 20:52:18 +0200

    animateArrowComponent: change direction and position by parameter 'positionArrow'. If position-parameter is left, css classes will add to component

--- src/app/landing-page/animate-arrow/animate-arrow.component.ts
+++ src/app/landing-page/animate-arrow/animate-arrow.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-animate-arrow',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './animate-arrow.component.html',
   styleUrl: './animate-arrow.component.scss'
 })
 export class AnimateArrowComponent {
-  position:number = 1;
+  /* position:number = 1; */
+  @Input() positionArrow:string = '';
 }
