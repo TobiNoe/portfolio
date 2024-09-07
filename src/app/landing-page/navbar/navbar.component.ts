d0a14493bcd0c8eebc3849bfaa84ffc2fc9fa1ee
Tobias Nölle <tobias.noelle@icloud.com>
2024-09-07 16:48:04 +0200

    navbarComponent: scss change image size of hamburger menu icon. ts change toggleMenu() function to render al Images in backwards direction.

--- src/app/landing-page/navbar/navbar.component.ts
+++ src/app/landing-page/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { NgClass } from '@angular/common';
 import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { DataServiceService } from '../../shared/services/data-service.service';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -12,12 +13,32 @@ import { DataServiceService } from '../../shared/services/data-service.service';
 export class NavbarComponent {
   data = inject(DataServiceService);
   @Output() openCloseEvent = new EventEmitter<boolean>();
-  hmenuImg:string = this.data.navbarImg[0];
-  menuIsToggled:boolean = false;
-  
+  hmenuImg: string = this.data.navbarImg[0];
+  menuIsToggled: boolean = false;
 
-  toggleMenu(){
-    this.hmenuImg = this.data.navbarImg[4];
+
+  toggleMenu() {
+    if (!this.menuIsToggled) {
+      for (let index = 0; index < this.data.navbarImg.length; index++) {
+        const img = this.data.navbarImg[index];
+
+        setTimeout(() => {
+          this.hmenuImg = img;
+        }, index * 100);
+      }
+    } else {
+      for (let index = 4; index >= 0; index--) {
+        const img = this.data.navbarImg[index];
+
+
+        setTimeout(() => {
+          this.hmenuImg = img;
+          console.log('img :>> ', this.hmenuImg);
+        }, (4 - index) * 100);
+      }
+    }
+
+    /* this.hmenuImg = this.data.navbarImg[4]; */
     this.menuIsToggled = !this.menuIsToggled;
 
     /* this.openCloseEvent.emit(!this.menuIsToggled); */
