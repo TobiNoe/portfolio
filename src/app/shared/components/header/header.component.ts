32049d4399f64983a18a1783cfafeed88a99c60f
Tobias Nölle <tobias.noelle@icloud.com>
2024-09-13 12:55:41 +0200

    headerComponent: copy code from navbarComponent

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DataServiceService } from '../../services/data-service.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  hmenuImg:string = "./../../../../../assets/img/navbar/burger_1.png"
+  data = inject(DataServiceService);
+  /* @Output() openCloseEvent = new EventEmitter<boolean>(); */
+  hmenuImg: string = this.data.navbarImg[0];
+  menuIsToggled: boolean = false;
+
+  toggleMenu() {
+    if (!this.menuIsToggled) {
+      this.renderImgForward();
+    } else {
+      this.renderImgBachward();
+    }
+    this.menuIsToggled = !this.menuIsToggled;
+    /* this.openCloseEvent.emit(!this.menuIsToggled); */
+  }
+
+  renderImgForward() {
+    for (let index = 0; index < this.data.navbarImg.length; index++) {
+      const img = this.data.navbarImg[index];
+      setTimeout(() => {
+        this.hmenuImg = img;
+      }, index * 100);
+    }
+  }
+
+  renderImgBachward(){
+    for (let index = 4; index >= 0; index--) {
+      const img = this.data.navbarImg[index];
+      setTimeout(() => {
+        this.hmenuImg = img;
+      }, (4 - index) * 100);
+    }
+  }
+
 }
