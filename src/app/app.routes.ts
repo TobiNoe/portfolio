d153dfefc413e330eb363b0c274bc2abbf2f613b
Tobias Nölle <tobias.noelle@icloud.com>
2024-09-13 12:35:46 +0200

    change structure of navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { NavigationComponent } from './shared/components/navigation/navigation.component';
 
 export const routes: Routes = [
 
-    { path: '', component: LandingPageComponent },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
     { path: 'navigation', component: NavigationComponent },
-    { path: 'aboutMe', component: LandingPageComponent }
+    { path: 'home', component: LandingPageComponent },
+    { path: 'legal', component: LegalNoticeComponent }
 ];
