6748661e2fb6b150c13a4b0680d192dc674b59ec
Tobias Nölle <tobias.noelle@icloud.com>
2024-10-06 12:49:35 +0200

    footerComponent: ts changed onSumbit() function to send mail with php script

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
@@ -18,6 +19,8 @@ export class FooterComponent {
   imgGitSrc: string = './assets/img/footer/github_button.svg';
   imgMailSrc: string = './assets/img/footer/email_button.svg';
   imgLinkedinSrc: string = './assets/img/footer/linkedin_button.svg';
+  mailTest = true;
+  http = inject(HttpClient);
 
   contactData = {
     name: "",
@@ -30,14 +33,46 @@ export class FooterComponent {
     console.log(this.policyChecked);
   }
 
-  onSubmit(ngForm: NgForm) {
+ /*  onSubmit(ngForm: NgForm) {
     if (ngForm.valid && ngForm.submitted && this.policyChecked) {
       console.log(this.contactData);
     }
-  }
+  } */
 
   scrollTop() {
     window.scroll({top: 0})
+  }
+
+  post = {
+    endPoint: 'https://tobias-noelle.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && this.policyChecked && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+            this.policyChecked = false;
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.policyChecked  && this.mailTest) {
+
+      ngForm.resetForm();
+      this.policyChecked = false;
+    }
   }
 
 }
