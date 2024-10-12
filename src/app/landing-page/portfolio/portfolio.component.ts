c0b45cf4e632d0961e014ef507030392c6893418
Tobias Nölle <tobias.noelle@icloud.com>
2024-10-12 11:33:55 +0200

    portfolioComponent: ts imported translateModule html added variables

--- src/app/landing-page/portfolio/portfolio.component.ts
+++ src/app/landing-page/portfolio/portfolio.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { PortfolioProjectComponent } from './portfolio-project/portfolio-project.component';
 import { DataServiceService } from '../../shared/services/data-service.service';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [PortfolioProjectComponent],
+  imports: [PortfolioProjectComponent, TranslateModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
