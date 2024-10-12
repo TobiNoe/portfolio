a6e8e0a40ebe6f8d79cbde5956e4eef5f83caeeb
Tobias Nölle <tobias.noelle@icloud.com>
2024-10-12 11:24:11 +0200

    mySkillsComponent: ts imported translateModule html added variables

--- src/app/landing-page/my-skills/my-skills.component.ts
+++ src/app/landing-page/my-skills/my-skills.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject} from '@angular/core';
 import { DataServiceService } from '../../shared/services/data-service.service';
 import { NgStyle } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-my-skills',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, TranslateModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss'
 })
